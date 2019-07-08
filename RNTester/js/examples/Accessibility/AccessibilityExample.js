54d944f0215a8fd74471d4c50b5cd3ec9e21f593
Kevin Gonzales <kegonzl@amazon.com>
2019-07-08 16:22:32 -0500

    changed View with multiple actions back to view from touchable

--- RNTester/js/examples/Accessibility/AccessibilityExample.js
+++ RNTester/js/examples/Accessibility/AccessibilityExample.js
@@ -436,7 +436,7 @@ class AccessibilityActionsExample extends React.Component {
         </RNTesterBlock>
 
         <RNTesterBlock title="View with multiple actions">
-          <TouchableOpacity
+          <View
             accessible={true}
             accessibilityActions={[
               {name: 'cut', label: 'cut label'},
@@ -457,7 +457,7 @@ class AccessibilityActionsExample extends React.Component {
               }
             }}>
             <Text>This view supports many actions.</Text>
-          </TouchableOpacity>
+          </View>
         </RNTesterBlock>
 
         <RNTesterBlock title="Adjustable with increment/decrement actions">
